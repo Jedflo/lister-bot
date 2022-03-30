fd69db2c11ce87d35f40cc14161f1681133b7466
Jedflo <jedflores091@gmail.com>
2022-03-30 13:20:40 +0800

    Added create, read, and delete file in utilities

--- src/main/java/me/jedflores/listerbot/Utilities.java
+++ src/main/java/me/jedflores/listerbot/Utilities.java
@@ -5,26 +5,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Utilities {
 
-//    public void createFile(String file_name){
-//        try{
-//            File myFile = new File(file_name);
-//            if(myFile.createNewFile()){
-//                System.out.println(file_name+" created successfully");
-//            }
-//            else{
-//                System.out.println(file_name+" already exist!");
-//            }
-//
-//        }
-//        catch(IOException e){
-//            System.out.println("An error occurred!");
-//            e.printStackTrace();
-//        }
-//    }
-//
     public void createFile(String file_name, String file_contents){
         try {
             FileWriter writer = new FileWriter(file_name);
@@ -38,12 +22,38 @@ public class Utilities {
         }
     }
 
+    public String readFile(String file_path){
+        String contents = "";
+        try{
+            File fileToRead = new File(file_path);
+            Scanner reader = new Scanner(fileToRead);
+            while(reader.hasNextLine()){
+                contents = reader.nextLine();
+            }
 
+        }
+        catch(FileNotFoundException e){
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+        return contents;
+    }
 
+    public void deleteFile(String file_path){
+        File myFile = new File(file_path);
+        if(myFile.delete()){
+            System.out.println(myFile.getName()+" Successfully deleted");
+        }
+        else {
+            System.out.println(myFile.getName()+" cannot be deleted");
+        }
+    }
 
     public static void main(String[] args) {
         Utilities util = new Utilities();
-        util.createFile("testing.txt","Testing testing");
+        //util.createFile("testing.txt","Testing testing");
+        String contents = util.readFile("testing.txt");
+        System.out.println(contents);
     }
 
 }
