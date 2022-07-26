051f1446b3aeb7e0f5c3afe2d56c21263c1093d4
JedFlo <jedflores091@gmail.com>
2022-07-26 19:10:02 +0800

    updated to enable getaway intent as per new discord rules

--- src/main/java/me/jedflores/listerbot/ListerBot.java
+++ src/main/java/me/jedflores/listerbot/ListerBot.java
@@ -7,20 +7,23 @@ import javax.security.auth.login.LoginException;
 import me.jedflores.listerbot.Tools.Utilities;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class ListerBot {
     //private static String token = "";
     
     public static void main (String[] args){
-        String token = Utilities.readFile("token.txt");
+        String token = Utilities.readFile("src/main/java/me/jedflores/listerbot/token.txt");
         token = token.replace("\n", "").replace("\r", "");
 
         JDABuilder builder = JDABuilder.createDefault(token);
         JDA jda;
         OneKQuestionsEvents pingpong = new OneKQuestionsEvents();
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
         builder.addEventListeners(pingpong);
         try {
             jda = builder.build();
+            StartupChecks.OneKQuestions();
         } catch (LoginException ex) {
             Logger.getLogger(ListerBot.class.getName()).log(Level.SEVERE, null, ex);
         }
