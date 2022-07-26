c641e3ec35d83c0d14b6afa601402427e9e10716
JedFlo <jedflores091@gmail.com>
2022-07-26 19:15:53 +0800

    automated the way Text2Database.java works. Text2Database reads a specified folder and inputs all text files within that folder to the database

--- src/main/java/me/jedflores/listerbot/Tools/Text2Database.java
+++ src/main/java/me/jedflores/listerbot/Tools/Text2Database.java
@@ -1,8 +1,14 @@
 package me.jedflores.listerbot.Tools;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static me.jedflores.listerbot.OneKQuestions.*;
 import static me.jedflores.listerbot.Tools.DatabaseTools.getConnection;
@@ -10,24 +16,63 @@ import static me.jedflores.listerbot.Tools.DatabaseTools.insertInto;
 
 public class Text2Database {
     public static void main(String[] args) {
-        String category = "vacation";
-        setQuestionCategory("Question Categories\\"+category+".txt");
-        List<String> questions = loadQuestions();
-        for (String q:questions) {
-            System.out.println(q);
-            try {
-                Connection con = getConnection();
-                String statement = "INSERT INTO questions(Question, Category) VALUES (?,?)";
-                PreparedStatement query = con.prepareStatement(statement);
-                query.setString(1,q);
-                query.setString(2,category);
-                query.executeUpdate();
-                query.close();
-                con.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+        textToDatabase("Question Categories");
+    }
+
+    public static boolean textToDatabase(String folder_path){
+        boolean out = true;
+        File folder = new File(folder_path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file: listOfFiles) {
+            if(file.isFile()){
+                String category = file.getName().replaceFirst("[.][^.]+$", "");
+                System.out.println(category+" file path : "+file.getPath());
+                List<String> questions = Utilities.fileToList(file.getPath());
+                System.out.println(questions.size()+" lines read from "+category+" file");
+                for (String q:questions) {
+                    System.out.println(q);
+                    try {
+                        Connection con = getConnection();
+                        String statement = "INSERT INTO questions(Question, Category) VALUES (?,?)";
+                        PreparedStatement query = con.prepareStatement(statement);
+                        query.setString(1,q);
+                        query.setString(2,category);
+                        query.executeUpdate();
+                        query.close();
+                        con.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(category+" DONE!");
+            } else if (file.isDirectory()) {
+                System.out.println(file.getName());
             }
         }
-        System.out.println(category+" DONE!");
+
+
+//        String category = "vacation";
+//        setQuestionCategory("Question Categories\\"+category+".txt");
+//        List<String> questions = loadQuestions();
+//        for (String q:questions) {
+//            System.out.println(q);
+//            try {
+//                Connection con = getConnection();
+//                String statement = "INSERT INTO questions(Question, Category) VALUES (?,?)";
+//                PreparedStatement query = con.prepareStatement(statement);
+//                query.setString(1,q);
+//                query.setString(2,category);
+//                query.executeUpdate();
+//                query.close();
+//                con.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        System.out.println(category+" DONE!");
+        return out;
     }
+
+
 }
